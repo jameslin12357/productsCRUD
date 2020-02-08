21027908db98f900c233452dc9e242565c85e18c
jameslin12357 <jameslin12357@outlook.com>
2020-02-08 11:52:08 +0800

    cm#2

--- client/indexCN.js
+++ client/indexCN.js
@@ -6,20 +6,20 @@ layui.use(['table','layer'], function(){
         elem: '#test'
         ,url:'http://localhost:3001/products'
         ,cols: [[
-            {field:'productId', title: 'ProductID'}
-            ,{field:'productName', title: 'Product Name'}
-            ,{field:'productLength', title: 'Product Length'}
-            ,{field:'productWidth', title: 'Product Width'}
-            ,{field:'productPrice', title: 'Product Price'}
-            ,{field:'productOrigin', title: 'Product Origin'}
-            ,{field:'productQuantity', title: 'Product Quantity'}
-            ,{field:'created', title: 'Created'}
-            ,{field:'updated', title: 'Updated'}
-            ,{fixed: 'right', width: 200, title: 'CRUD', align:'center', toolbar: '#barDemo'}        
+            {field:'productId', title: '产品ID'}
+            ,{field:'productName', title: '产品名称'}
+            ,{field:'productLength', title: '产品长度'}
+            ,{field:'productWidth', title: '产品宽度'}
+            ,{field:'productPrice', title: '产品价格'}
+            ,{field:'productOrigin', title: '产品产地'}
+            ,{field:'productQuantity', title: '产品数量'}
+            ,{field:'created', title: '创建日期'}
+            ,{field:'updated', title: '编辑日期'}
+            ,{fixed: 'right', width: 200, title: '操作', align:'center', toolbar: '#barDemo'}        
 ]]
         ,toolbar: '#barDemo2'
         ,page: true
-        ,title: "PRODUCTCRUD"
+        ,title: "产品CRUD"
     });
 //监听工具条 
 table.on('tool(test)', function(obj) { //注：tool 是工具条事件名，test 是 table 原始容器的属性 lay-filter="对应的值"
@@ -32,17 +32,17 @@ table.on('tool(test)', function(obj) { //注：tool 是工具条事件名，test
             btn: [],
             maxWidth: 750,
             shade: 0,
-            title: "Product",
+            title: "产品",
             content: `<div>
-                             <p><span>ProductID:</span> <span>${data["productId"]}</span></p>
-                             <p><span>Product Name:</span> <span>${data["productName"]}</span></p>
-                             <p><span>Product Length:</span> <span>${data["productLength"]}</span></p>
-                             <p><span>Product Width:</span> <span>${data["productWidth"]}</span></p>
-                             <p><span>Product Price:</span> <span>${data["productPrice"]}</span></p>
-                             <p><span>Product Origin:</span> <span>${data["productOrigin"]}</span></p>
-                             <p><span>Product Quantity:</span> <span>${data["productQuantity"]}</span></p>
-                             <p><span>Created:</span> <span>${data["created"]}</span></p>
-                             <p><span>Updated:</span> <span>${data["updated"]}</span></p>
+                             <p><span>产品ID:</span> <span>${data["productId"]}</span></p>
+                             <p><span>名称:</span> <span>${data["productName"]}</span></p>
+                             <p><span>长度:</span> <span>${data["productLength"]}</span></p>
+                             <p><span>宽度:</span> <span>${data["productWidth"]}</span></p>
+                             <p><span>价格:</span> <span>${data["productPrice"]}</span></p>
+                             <p><span>产地:</span> <span>${data["productOrigin"]}</span></p>
+                             <p><span>数量:</span> <span>${data["productQuantity"]}</span></p>
+                             <p><span>创建日期:</span> <span>${data["created"]}</span></p>
+                             <p><span>编辑日期:</span> <span>${data["updated"]}</span></p>
                             </div>`
         });
         //layer.msg(JSON.stringify(data));
@@ -52,8 +52,8 @@ table.on('tool(test)', function(obj) { //注：tool 是工具条事件名，test
         layer.open({
             btn: [],
             shade: 0,
-            title: "Delete Product",
-            content: `<div><div class="mb-15 tc">Confirm delete product?</div><div class="tr"><button id="buttonDelete" type="submit" class="layui-btn layui-btn-danger">Delete</button></div></div>`
+            title: "删除产品",
+            content: `<div><div class="mb-15 tc">确定删除产品?</div><div class="tr"><button id="buttonDelete" type="submit" class="layui-btn layui-btn-danger">删除</button></div></div>`
         });
         document.getElementById("buttonDelete").addEventListener("click", function (e) {
             deleteProductPost();
@@ -63,45 +63,45 @@ table.on('tool(test)', function(obj) { //注：tool 是工具条事件名，test
         layer.open({
             btn: [],
             shade: 0,
-            title: "Update Product",
+            title: "编辑产品",
             content: `<form class="layui-form layui-form-pane" action="">
                               <div class="layui-form-item">
-                    <label class="layui-form-label">Name</label>
+                    <label class="layui-form-label">名称</label>
                     <div class="layui-input-inline">
                       <input type="text" name="productName" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input" required value="${data["productName"]}">
                     </div>
                   </div>
                   <div class="layui-form-item">
-                    <label class="layui-form-label">Length</label>
+                    <label class="layui-form-label">长度</label>
                     <div class="layui-input-inline">
                       <input type="text" name="productLength" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input"  required value="${data["productLength"]}">
                     </div>
                   </div>
                      <div class="layui-form-item">
-                    <label class="layui-form-label">Width</label>
+                    <label class="layui-form-label">宽度</label>
                     <div class="layui-input-inline">
                       <input type="text" name="productWidth" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input" required value="${data["productWidth"]}">
                     </div>
                   </div>
                   <div class="layui-form-item">
-                    <label class="layui-form-label">Price</label>
+                    <label class="layui-form-label">价格</label>
                     <div class="layui-input-inline">
                       <input type="text" name="productPrice" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input"  required value="${data["productPrice"]}">
                     </div>
                   </div>
                      <div class="layui-form-item">
-                    <label class="layui-form-label">Origin</label>
+                    <label class="layui-form-label">产地</label>
                     <div class="layui-input-inline">
                       <input type="text" name="productOrigin" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input" required value="${data["productOrigin"]}">
                     </div>
                   </div>
                   <div class="layui-form-item">
-                    <label class="layui-form-label">Quantity</label>
+                    <label class="layui-form-label">数量</label>
                     <div class="layui-input-inline">
                       <input type="text" name="productQuantity" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input" required value="${data["productQuantity"]}">
                     </div>
                   </div>
- <button type="submit" class="layui-btn layui-btn-normal fr">Save</button>
+ <button type="submit" class="layui-btn layui-btn-normal fr">保存</button>
 </form>`
         });
         document.getElementsByTagName("form")[0].addEventListener("submit", function (e) {
@@ -120,45 +120,45 @@ table.on('toolbar(test)', function(obj){
          layer.open({
             btn: [],
             shade: 0,
-            title: "Create Product",
+            title: "新建产品",
             content: `<form class="layui-form layui-form-pane" action="">
                    <div class="layui-form-item">
-                    <label class="layui-form-label">Name</label>
+                    <label class="layui-form-label">名称</label>
                     <div class="layui-input-inline">
-                      <input type="text" name="productName" lay-verify="required" placeholder="Enter name" autocomplete="off" class="layui-input" required>
+                      <input type="text" name="productName" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input" required>
                     </div>
                   </div>
                   <div class="layui-form-item">
-                    <label class="layui-form-label">Length</label>
+                    <label class="layui-form-label">长度</label>
                     <div class="layui-input-inline">
-                      <input type="text" name="productLength" lay-verify="required" placeholder="Enter length" autocomplete="off" class="layui-input"  required>
+                      <input type="text" name="productLength" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input"  required>
                     </div>
                   </div>
                      <div class="layui-form-item">
-                    <label class="layui-form-label">Width</label>
+                    <label class="layui-form-label">宽度</label>
                     <div class="layui-input-inline">
-                      <input type="text" name="productWidth" lay-verify="required" placeholder="Enter width" autocomplete="off" class="layui-input" required>
+                      <input type="text" name="productWidth" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input" required>
                     </div>
                   </div>
                   <div class="layui-form-item">
-                    <label class="layui-form-label">Price</label>
+                    <label class="layui-form-label">价格</label>
                     <div class="layui-input-inline">
-                      <input type="text" name="productPrice" lay-verify="required" placeholder="Enter price" autocomplete="off" class="layui-input"  required>
+                      <input type="text" name="productPrice" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input"  required>
                     </div>
                   </div>
                      <div class="layui-form-item">
-                    <label class="layui-form-label">Origin</label>
+                    <label class="layui-form-label">产地</label>
                     <div class="layui-input-inline">
-                      <input type="text" name="productOrigin" lay-verify="required" placeholder="Enter origin" autocomplete="off" class="layui-input" required>
+                      <input type="text" name="productOrigin" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input" required>
                     </div>
                   </div>
                   <div class="layui-form-item">
-                    <label class="layui-form-label">Quantity</label>
+                    <label class="layui-form-label">数量</label>
                     <div class="layui-input-inline">
-                      <input type="text" name="productQuantity" lay-verify="required" placeholder="Enter quantity" autocomplete="off" class="layui-input"  required>
+                      <input type="text" name="productQuantity" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input"  required>
                     </div>
                   </div>
- <button type="submit" class="layui-btn layui-btn-normal fr">Save</button>
+ <button type="submit" class="layui-btn layui-btn-normal fr">保存</button>
 </form>`
         });
         document.getElementsByTagName("form")[0].addEventListener("submit", function (e) {
@@ -188,7 +188,7 @@ function editProductPost(){
         success: function (data) {
             if (data.code === 1){
                 layer.closeAll();
-                layer.msg("Product updated");
+                layer.msg("产品已编辑");
                 table.reload('test', {
   //url: '/api/table/search'
   //,where: {} //设定异步数据接口的额外参数
@@ -210,7 +210,7 @@ function deleteProductPost() {
         success: function (data) {
             if (data.code === 1) {
                 layer.closeAll();
-                layer.msg("Product deleted");
+                layer.msg("产品已删除");
                 table.reload('test', { 
   //url: '/api/table/search'
   //,where: {} //设定异步数据接口的额外参数
@@ -244,7 +244,7 @@ function createProductPost(){
         success: function (data) {
             if (data.code === 1){
                 layer.closeAll();
-                layer.msg("Product created");
+                layer.msg("产品已创建");
                 //$('#dg').datagrid('reload');
                  table.reload('test', {
   //url: '/api/table/search'
